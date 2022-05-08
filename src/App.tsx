35d93b94d9da03161ba13959606b265f1b219a65
d-nazir <darryl@ctrlup.io>
2022-05-09 01:33:29 +0400

    feat: 🎸 capture mouse angle
    --rainbow made of linear gradient as iridescence, pass --angle to :root

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
+  let ctrlup = useRef<HTMLDivElement>(null);
+  const root = document.documentElement;
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!ctrlup.current) return;
+    var rect = ctrlup.current.getBoundingClientRect();
+    let x = e.clientX - (rect.left + rect.right) / 2;
+    let y = e.clientY - (rect.top + rect.bottom) / 2;
+    let angle = (Math.atan2(x, y) * 360) / Math.PI;
+    root.style.setProperty("--angle", `${angle}deg`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onMouseMove={onMouseMove}>
+      <div className="ctrlup-container" ref={ctrlup}>
+        <span className="ctrlup">CtrlUp</span>
+      </div>
     </div>
   );
 }
